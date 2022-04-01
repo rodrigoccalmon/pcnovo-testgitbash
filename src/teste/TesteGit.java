3aadc48b86891685f4d282a9b57949630552dd2b
Rodrigo Calmon <rodrigo.ccalmon@gmail.com>
2022-04-01 06:19:19 -0300

    testando commit pc novo

---
+++ src/teste/TesteGit.java
@@ -0,0 +1,9 @@
+package teste;
+
+public class TesteGit {
+
+	public static void main(String[] args) {
+		System.out.println("Olá, mundo!");
+	}
+
+}
---
+++ src/teste/teste.java
@@ -0,0 +1,9 @@
+package teste;
+
+public class teste {
+
+	public static void main(String[] args) {
+		System.out.println("olá, viceri!");
+	}
+
+}
